2b072c38bbb5e2d3980a468d09a1bf30bed05e17
Anna <annasofie.lunde@gmail.com>
2022-05-10 13:27:50 +0200

    ready for tuning of destruction degree

--- config/main_config.py
+++ config/main_config.py
@@ -56,27 +56,27 @@ initial_iterations = 100
 reopt_iterations = 100
 
 # Weight of ride time in objective function
-alpha = 1
+alpha = 2
 
 # Weight of deviation in objective function
 beta = 100
 
 # Start temperature control parameter
-initial_Z = 0.3
-reopt_Z = 0.3
+initial_Z = 0.8
+reopt_Z = 0.8
 
 # Cooling rate
-cooling_rate = 0.96
+cooling_rate = 0.98
 
 # Weight scores
-weights = [10, 5, 1, 0]
+weights = [15, 10, 5, 0]
 
 # Reaction factor
-reaction_factor = 0.6
+reaction_factor = 0.7
 
 # Number of destroy/repair pairs before updating weights in initial
-N_U_init = 0.3
-N_U_reopt = 0.3
+N_U_init = 0.2
+N_U_reopt = 0.2
 
 # Destruction degree
 destruction_degree = 0.4
@@ -84,4 +84,9 @@ destruction_degree = 0.4
 
 # Running data
 # "comp_instance_1_20220110", "comp_instance_2_20211007","comp_instance_3_20211215"
-test_instance = "comp_instance_1_20220110"
+
+# "test_instance_small_1_20210703", "test_instance_small_2_20210724", "test_instance_small_3_20210918",
+# "test_instance_medium_1_20210706",test_instance_medium_2_20210830", "test_instance_medium_3_20211015",
+# "test_instance_large_1_20211005","test_instance_large_2_20211014", "test_instance_large_3_20220112"
+test_instances = ["test_instance_small_1_20210703", "test_instance_small_2_20210724", "test_instance_small_3_20210918", "test_instance_medium_1_20210706", "test_instance_medium_2_20210830", "test_instance_medium_3_20211015", "test_instance_large_1_20211005",
+                  "test_instance_large_2_20211014", "test_instance_large_3_20220112"]
--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from copy import copy
-import pandas as pd
-from decouple import config
-import sys
-import numpy.random as rnd
-import traceback
-from heuristic.construction.construction import ConstructionHeuristic
-from heuristic.improvement.alns import ALNS
-from config.main_config import *
-from heuristic.improvement.initial.initial_operators import Operators
-from heuristic.improvement.reopt.reopt_operators import ReOptOperators
-from heuristic.improvement.simulated_annealing import SimulatedAnnealing
-from simulation.simulator import Simulator
-from heuristic.improvement.reopt.disruption_updater import DisruptionUpdater
 from heuristic.improvement.reopt.new_request_updater import NewRequestUpdater
+from heuristic.improvement.reopt.disruption_updater import DisruptionUpdater
+from simulation.simulator import Simulator
+from heuristic.improvement.simulated_annealing import SimulatedAnnealing
+from heuristic.improvement.reopt.reopt_operators import ReOptOperators
+from heuristic.improvement.initial.initial_operators import Operators
+from config.main_config import *
+from heuristic.improvement.alns import ALNS
+from heuristic.construction.construction import ConstructionHeuristic
+#from profiling.profiler import Profile
+import cProfile
+import traceback
+import numpy.random as rnd
+import sys
+from decouple import config
+import pandas as pd
+from copy import copy
 
 
-def main(test_instance, test_instance_date, run, repair_removed, destroy_removed):
+def main(test_instance, test_instance_date):
     constructor, simulator = None, None
 
     try:
-        # TRACKING
-        start_time = datetime.now()
-        df_operators = []
 
         # CUMULATIVE OBJECTIVE
         cumulative_rejected, cumulative_recalibration, cumulative_objective = 0, timedelta(
@@ -35,6 +34,8 @@ def main(test_instance, test_instance_date, run, repair_removed, destroy_removed
         initial_route_plan, initial_objective, initial_infeasible_set = constructor.construct_initial()
 
         # IMPROVEMENT OF INITIAL SOLUTION
+        random_state = rnd.RandomState()
+
         criterion = SimulatedAnnealing(cooling_rate)
 
         alns = ALNS(weights, reaction_factor, initial_route_plan, initial_objective, initial_infeasible_set, criterion,
@@ -42,13 +43,13 @@ def main(test_instance, test_instance_date, run, repair_removed, destroy_removed
 
         operators = Operators(alns)
 
-        alns.set_operators(operators, repair_removed, destroy_removed)
+        alns.set_operators(operators)
 
         # Run ALNS
         delayed = (False, None, None)
 
-        df_operators, current_route_plan, current_objective, current_infeasible_set, _ = alns.iterate(
-            initial_iterations, initial_Z, None, None, None, delayed, False, df_operators, run)
+        current_route_plan, current_objective, current_infeasible_set, _ = alns.iterate(
+            initial_iterations, initial_Z, None, None, None, delayed, False)
 
         if current_infeasible_set:
             cumulative_rejected = len(current_infeasible_set)
@@ -57,134 +58,22 @@ def main(test_instance, test_instance_date, run, repair_removed, destroy_removed
             print("Number of rejected", cumulative_rejected)
             current_infeasible_set = []
 
+        '''
         # Recalibrate current solution
         current_route_plan = constructor.recalibrate_solution(
             current_route_plan)
 
         delta_dev_objective = constructor.get_delta_objective(
-            current_route_plan, [i for i in range(cumulative_rejected)], current_objective)
+            current_route_plan, [], current_objective)
         cumulative_recalibration += delta_dev_objective
         current_objective -= delta_dev_objective
+        '''
+        ride_time_objective, deviation_objective, rejected_objective = constructor.print_objective(
+            current_route_plan, [i for i in range(cumulative_rejected)])
 
-        print("Initial objective", current_objective.total_seconds())
-        print("Initial rejected", cumulative_rejected)
-
-        # SIMULATION
-        print("Start simulation")
-        sim_clock = datetime.strptime(
-            test_instance_date, "%Y-%m-%d %H:%M:%S")
-        simulator = Simulator(sim_clock)
-        new_request_updater = NewRequestUpdater(
-            constructor)
-        disruption_updater = DisruptionUpdater(new_request_updater)
-        first_iteration, rejected = True, []
-        print("Length of disruption stack", len(simulator.disruptions_stack))
-        while len(simulator.disruptions_stack) > 0:
-            start_time = datetime.now()
-            prev_inf_len = cumulative_rejected
-            delayed, delay_deltas = (False, None, None), []
-            i = 0
-            prev_objective = current_objective
-            rejection = False
-
-            # use correct data path
-            if not first_iteration:
-                disruption_type, disruption_time, disruption_info = simulator.get_disruption(
-                    current_route_plan, config("data_simulator_path"), first_iteration)
-            else:
-                disruption_type, disruption_time, disruption_info = simulator.get_disruption(
-                    current_route_plan, config("data_processed_path"), first_iteration)
-                first_iteration = False
-            # updates before heuristic
-            disrupt = (False, None)
-            if disruption_type == 4:  # No disruption
-                continue
-            elif disruption_type == 0:  # Disruption: new request
-                current_route_plan, vehicle_clocks = disruption_updater.update_route_plan(
-                    current_route_plan, disruption_type, disruption_info, disruption_time)
-                updated_objective = new_request_updater.new_objective(
-                    current_route_plan, [], False)
-                current_route_plan = disruption_updater.filter_route_plan(
-                    current_route_plan, vehicle_clocks)  # Filter route plan
-                filter_objective = new_request_updater.new_objective(
-                    current_route_plan, [], False)
-                cumulative_objective = copy(
-                    cumulative_objective) + copy(updated_objective) - copy(filter_objective)
-                current_route_plan, current_objective, current_infeasible_set, vehicle_clocks, rejection, rid = new_request_updater.\
-                    greedy_insertion_new_request(
-                        current_route_plan, current_infeasible_set, disruption_info, disruption_time, vehicle_clocks, i, filter_objective)
-                disruption_type = str(disruption_type) + "_False"
-                if rejection:
-                    disruption_type = str(disruption_type) + "_True"
-                    rejected.append(rid)
-                    cumulative_rejected += 1
-                    for i in range(1, N_R+1):
-                        current_route_plan, current_objective, current_infeasible_set, vehicle_clocks, rejection, rid = new_request_updater.\
-                            greedy_insertion_new_request(
-                                current_route_plan, current_infeasible_set, disruption_info, disruption_time, vehicle_clocks, i, filter_objective)
-                        if not rejection:
-                            rejected.remove(rid)
-                            cumulative_rejected -= 1
-                            break
-                current_infeasible_set = []
-
-            else:
-                current_route_plan, vehicle_clocks = disruption_updater.update_route_plan(
-                    current_route_plan, disruption_type, disruption_info, disruption_time)
-                updated_objective = new_request_updater.new_objective(
-                    current_route_plan, [], False)
-                current_route_plan = disruption_updater.filter_route_plan(
-                    current_route_plan, vehicle_clocks)  # Filter route plan
-                filter_objective = new_request_updater.new_objective(
-                    current_route_plan, [], False)
-                cumulative_objective = copy(
-                    cumulative_objective) + copy(updated_objective) - copy(filter_objective)
-                current_objective = new_request_updater.new_objective(
-                    current_route_plan, current_infeasible_set, False)
-                if disruption_type == 2 or disruption_type == 3:  # Disruption: cancel or no show
-                    index_removed = [(disruption_info[3], disruption_info[0], disruption_info[1]),
-                                     (disruption_info[4], disruption_info[0], disruption_info[2])]
-                    disrupt = (True, index_removed)
-                elif disruption_type == 1:  # Disruption: delay
-                    delayed = (True, disruption_info[0], disruption_info[1])
-                    delay_deltas.append(current_objective)
-
-            if not rejection:
-                # Heuristic
-                criterion = SimulatedAnnealing(cooling_rate)
-
-                alns = ALNS(weights, reaction_factor, current_route_plan, current_objective, current_infeasible_set,
-                            criterion,
-                            destruction_degree, new_request_updater, rnd_state=rnd.RandomState())
-
-                operators = ReOptOperators(
-                    alns, disruption_time, vehicle_clocks)
-
-                alns.set_operators(operators, repair_removed, destroy_removed)
-
-                # Run ALNS
-                df_operators, current_route_plan, current_objective, current_infeasible_set, still_delayed_nodes = alns.iterate(
-                    reopt_iterations, reopt_Z, disrupt[0], disrupt[1], disruption_time, delayed, True, df_operators, run)
-
-                if delayed[0]:
-                    delay_deltas[-1] = delay_deltas[-1] - current_objective
-                    current_route_plan = disruption_updater.recalibrate_solution(
-                        current_route_plan, disruption_info, still_delayed_nodes)
-
-                    delta_dev_objective = new_request_updater.get_delta_objective(
-                        current_route_plan, current_infeasible_set, current_objective)
-                    cumulative_recalibration += delta_dev_objective
-                    current_objective -= delta_dev_objective
-
-            total_objective = new_request_updater.total_objective(current_objective,
-                                                                  cumulative_objective,
-                                                                  cumulative_recalibration, cumulative_rejected, rejection)
-
-            print("Disruption type", str(disruption_type),
-                  total_objective.total_seconds())
-
-        print("End simulation")
-        print("Rejected rids", rejected)
+        total_objective = constructor.total_objective(current_objective, cumulative_objective,
+                                                      cumulative_recalibration)
+        return total_objective, cumulative_rejected, rejected_objective, deviation_objective, ride_time_objective
 
     except Exception as e:
         print("ERROR:", e)
@@ -198,56 +87,36 @@ def main(test_instance, test_instance_date, run, repair_removed, destroy_removed
         print("File name: ", filename)
         print("Line number: ", line_number)
 
-    return df_operators
-
 
 if __name__ == "__main__":
     """
-    # Profilinga
+    # Profiling
     profile = Profile()
     cProfile.run('main()', 'profiling/restats')
     profile.display()
-    """
 
     # Generate test instance datetime from filename
     test_instance_d = test_instance.split(
-        "/")[-1].split("_")[-1].split(".")[0]
-    test_instance_date = test_instance_d[0:4] + "-" + \
-        test_instance_d[4:6] + "-" + \
-        test_instance_d[6:8] + " 10:00:00"
-    print("Test instance:", test_instance)
+                "/")[-1].split("_")[-1].split(".")[0]
+            test_instance_date = test_instance_d[0:4] + "-" + \
+                test_instance_d[4:6] + "-" + \
+                test_instance_d[6:8] + " 10:00:00"
+    """
+    runs = 10
+    print("Cooling rate:", cooling_rate)
 
-    repair_removed = None
-    destroy_removed = None
-    runs = 1
-    df_operators_runs = []
-    for run in range(runs):
-        df_operators = main(
-            test_instance, test_instance_date, run, repair_removed, destroy_removed)
-        df_operators_runs.append(pd.DataFrame(df_operators, columns=[
-            "Run", "Initial", "Iteration", "Destroy Operator", "Repair Operator", "Destroy Weight", "Repair Weight", "Update destroy weight score", "Update repair weight score", "Destroy Used", "Repair Used", "Runtime", "Updated this round", "Best Objective"]))
+    for test_instance in test_instances:
+        tracking = []
+        for run in range(runs):
+            start_time = datetime.now()
 
-    df_operators_total = pd.concat(df_operators_runs)
-    df_operators_total.to_csv(
-        config("run_path") + test_instance + "run: " + "1" + ".csv")
+            total_objective, rejected, rejected_objective, deviation_objective, ride_time_objective = main(
+                test_instance, "2021-05-10 10:00:00")
+            tracking.append([run, total_objective.total_seconds(),
+                             (datetime.now() - start_time).total_seconds(), rejected, rejected_objective, deviation_objective.total_seconds(), ride_time_objective.total_seconds()])
 
-    # df_operators_total.to_csv(
-    #    config("run_path") + test_instance + "_impact_removed_repair:" + repair_removed + "_impact_removed_destroy:" + destroy_removed + ".csv")
-
+        df_tracking = pd.DataFrame(
+            tracking, columns=["Run", "Current Objective", "Solution Time", "Rejected", "Norm Rejected Objective", "Norm Deviation Objective", "Norm Ride Time Objective"])
+        df_tracking.to_csv(config("tuning_path") + "param_tuning_" + str(cooling_rate) +
+                           "_" + test_instance + ".csv")  # Path:  param_tuning_iterations_instance
     print("DONE WITH ALL RUNS")
-
-"""
-TODO:
-    - Add index to removed repair operator:
-        - None = none removed 
-        - 0 = greedy_repair
-        - 1 = regret_2_repair
-        - 2 = regret_3_repair
-    - Add index to removed destroy operator:
-        - None = none removed
-        - 0 = random_removal
-        - 1 = time_related_removal
-        - 2 = distance_related_removal
-        - 3 = related_removal
-        - 4 = worst_deviation_removal
-"""
