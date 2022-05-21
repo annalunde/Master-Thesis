043615622cd607edb96f2fbeb9b1ee98b8256a3a
Anna <annasofie.lunde@gmail.com>
2022-05-21 13:41:09 +0200

    ⚡️ configure model and operators

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     naive = False
     adaptive = False
     repair_removed = None
-    destroy_removed = None
+    destroy_removed = [0, 2]
     runs = 5
     standby = 0
 
