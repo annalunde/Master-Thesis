6c49b1b66aa990a841f1dd0b788cb21c0213c6da
Anna <annasofie.lunde@gmail.com>
2022-05-21 11:53:25 +0200

    ✨ set operators to be used

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
     print("Test instance:", test_instance)
 
     repair_removed = None
-    destroy_removed = None
+    destroy_removed = [0, 2]
     runs = 5
     df_requests_runs, df_runtime_runs, df_operators_runs = [], [], []
     for run in range(runs):
