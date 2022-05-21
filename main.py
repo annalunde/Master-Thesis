eda864416e741d7183a186a2fb40be4cbeb9a07f
Anna <annasofie.lunde@gmail.com>
2022-05-21 13:13:20 +0200

    ready for heuristic vs naive 3.0

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ if __name__ == "__main__":
         test_instance_d[4:6] + "-" + \
         test_instance_d[6:8] + " 10:00:00"
 
-    naive = True
+    naive = False
     adaptive = False
     repair_removed = None
     destroy_removed = [0, 2]
