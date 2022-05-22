da1175379733b3e8997e5670e62b608a29795bf7
Anna <annasofie.lunde@gmail.com>
2022-05-22 14:16:25 +0200

    ⚡️ will this work

--- main.py
+++ main.py
@@ -279,9 +279,15 @@ if __name__ == "__main__":
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('--run', type=int)
+    parser.add_argument('--branch', type=str)
+    parser.add_argument('--instance', type=str)
     args = parser.parse_args()
 
     run = args.run
+    branch = args.branch
+    print(f'Config says instance {test_instance}')
+    test_instance = args.instance
+    print(f'Replaced to argument instance {args.instance}')
 
     # Generate test instance datetime from filename
     test_instance_d = test_instance.split(
@@ -310,7 +316,7 @@ if __name__ == "__main__":
 
     df_track_run = pd.concat(df_runs)
     df_track_run.to_csv(
-        config("run_path") + "Cancel_Disruptions" + "Run:" + run + ":" + test_instance + "analysis" + ".csv")
+        config("run_path") + branch + "Run:" + run + ":" + test_instance + "analysis" + ".csv")
 
     print("DONE WITH ALL RUNS")
 
---
+++ main.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+export cores=16
+export i=0
+export pids=""
+
+for instance in "comp_instance_1_20220110 comp_instance_2_20211007 comp_instance_3_20211215"
+do
+    for run in $(seq 5)
+    do
+        # Checkout the branch and start a run in the background
+        echo "Starting run $run on branch $branch"
+        #git checkout $branch
+        #sleep 5
+        python main.py --run $run --instance $instance --branch $branch &
+        #sleep 5
+
+        # Add the latest subprocess to the list of PIDs
+        pids="$pids $!"
+        i=$((i + 1))
+        # Wait for all of the runs to complete before starting a new set.
+        # This is to avoid starting more processes than there are cores. Could've done this better
+        if [ $i -eq $cores ]; then
+            echo "Waiting for tasks to finish before starting new"
+            wait $pids
+            i=$((0))
+            pids=""
+        fi
+    done
+done
+
+# Wait for all remaining jobs to finish.
+echo "Waiting for last set of tasks to finish"
+wait $pids
