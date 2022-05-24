b917c21e6ab90f5908838b13a638242dc81472ba
Anna <annasofie.lunde@gmail.com>
2022-05-24 10:44:12 +0200

    ready for comp with fixed cpt

--- main.py
+++ main.py
@@ -314,7 +314,7 @@ if __name__ == "__main__":
 
     df_track_run = pd.concat(df_runs)
     df_track_run.to_csv(
-        config("run_path") + branch + "Run:" + str(run) + ":" + test_instance + "analysis" + ".csv")
+        config("run_path") + branch + "Disruptions_Run:" + str(run) + ":" + test_instance + "analysis" + ".csv")
 
     print("DONE WITH ALL RUNS")
 
