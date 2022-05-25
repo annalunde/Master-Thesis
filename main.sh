cf5773aecd20098925593840cd892311d7f3b621
Anna <annasofie.lunde@gmail.com>
2022-05-25 10:08:57 +0200

    last run on instance3

--- main.sh
+++ main.sh
@@ -3,9 +3,9 @@ export cores=16
 export i=0
 export pids=""
 
-for instance in comp_instance_1_20220110 comp_instance_2_20211007 comp_instance_3_20211215
+for instance in comp_instance_3_20211215
 do
-    for run in $(seq 5)
+    for run in $(seq 2)
     do
         # Checkout the branch and start a run in the background
         echo "Starting run $run on instance $instance on branch $1"
