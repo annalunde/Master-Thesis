b26929e475e5ae40f157b3bf4b376797115b57b8
Anna <annasofie.lunde@gmail.com>
2022-06-02 17:00:39 +0200

    ready for naive with all info

--- main.py
+++ main.py
@@ -300,7 +300,7 @@ if __name__ == "__main__":
         test_instance_d[4:6] + "-" + \
         test_instance_d[6:8] + " 10:00:00"
 
-    naive = False
+    naive = True
     adaptive = True
     repair_removed = None
     destroy_removed = [0, 2]
